96effeadbdaf1488504275b02ad6c37a71ea49f9
Chidiebere-Aginam <chidiebereaginam@yahoo.com>
2022-11-07 21:17:03 +0100

    Print a string

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,13 @@
+int _putchar(char c);
+void _puts_recursion(char *s);
+void _print_rev_recursion(char*);
+int _strlen_recursion(char*);
+int factorial(int);
+int _pow_recursion(int, int);
+int _sqrt_recursion(int);
+int is_prime_number(int);
+int wildcmp(char*, char*);
+int is_palindrome(char*);
+int get_length(char*);
+int my_pal(char*, int);
+int findsrc(char*, char, int, int);

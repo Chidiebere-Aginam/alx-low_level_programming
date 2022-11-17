98d14e04a3da5f6bfd2db6037dc8e358a352026b
Chidiebere-Aginam <chidiebereaginam@yahoo.com>
2022-11-17 20:33:49 +0100

    Sum

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -14,13 +14,13 @@ int sum_them_all(const unsigned int n, ...)
 
 	if (n == 0)
 		return (0);
-	{
+	
 		va_start(arguments, n);
 		for (i = 0; i < n; i++)
 		{
 			sum += va_arg(arguments, int);
 		}
 		va_end(arguments);
-	}
+	
 	return (sum);
 }

a91150db3cc730df0d7343e9d867dedb74c34399
Chidiebere-Aginam <chidiebereaginam@yahoo.com>
2022-12-05 21:36:01 +0100

    File I/O

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,37 +1,46 @@
 #include "main.h"
+
 /**
- * read_textfile - reads a text file and prints it to the standard output
- * @filename: name of the file to be read
- * @letters: number of letters to read and print
- * Return: the number of letters printed, or 0 if it failed
+ * read_textfile - reads text file and prints it to the POSIX standard output.
+ * @filename: name of the file to be read and printed.
+ * @letters: is the number of letters it should read and print.
+ * Return: the actual number of letters it could read and print.
  */
+
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd;
-	int i, y;
-	char *buf;
-	if (!filename)
+	int fd = -1;
+	char buf[1024];
+	ssize_t r = 0, w = 0;
+	ssize_t ra = 0, bsize = sizeof(buf) / sizeof(buf[0]);
+
+	if (filename == NULL)
 		return (0);
 	fd = open(filename, O_RDONLY);
-	if (fd < 0)
+	if (fd == -1)
 		return (0);
-	buf = malloc(sizeof(char) * letters);
-	if (!buf)
-		return (0);
-	i = read(fd, buf, letters);
-	if (i < 0)
+
+	while (letters > 0)
 	{
-		free(buf);
-		return (0);
+		if (letters < (size_t)bsize)
+			bsize = (ssize_t)letters;
+
+		ra = read(fd, buf, bsize);
+		if (ra <= 0)
+			return (0);
+		r = r + ra;
+		if (ra < bsize)
+		{
+			w = write(STDOUT_FILENO, buf, ra);
+			if (w != ra)
+				return (0);
+			break;
+		}
+		w = write(STDOUT_FILENO, buf, bsize);
+		if (w != bsize)
+			return (0);
+		letters = letters - bsize;
 	}
-	buf[i] = '\0';
 	close(fd);
-	y = write(STDOUT_FILENO, buf, i);
-	if (y < 0)
-	{
-		free(buf);
-		return (0);
-	}
-	free(buf);
-	return (y);
+	return (r);
 }
